e145f4c877e175725d0a377d644fd34d7fc7d8e5
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-04-11 22:35:27 +0300

    Pajungiame, kad kompiliuotų React'o JSX failus
    Iš assets/js eis į build/js/app.js, nes taip parašėme webpack.config.js
    
    scripts/frontend.sh
      yarn run encore dev --watch
    
    Rezultato ieškoti:
      build/js/app.js
    
    Dokumentacija:
      https://symfony.com/doc/current/frontend/encore/reactjs.html
    
    fixup!  Pajungiame, kad kompiliuotų React'o JSX failus

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,10 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
+    .addEntry('app', [
+        './assets/js/app.js',
+        './assets/js/react.jsx'
+    ])
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -49,6 +52,7 @@ Encore
 
     // enables Sass/SCSS support
     .enableSassLoader()
+    .enableReactPreset()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()

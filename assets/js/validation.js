0bac73a32fc9a01b9b245b18e787c15b7a098957
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-04-12 01:01:39 +0300

    Siųnčiame AJAX užklausą
    Keičiant laukelio turinį, grąžina ":)"
    Rankomis galima pasibandyti pakeisti, ar pagauna ir klaidas.
    
    Bet jau turime sujungtą Frontend'ą su Backend'u.
    
    Dokumentacija:
      https://www.npmjs.com/package/axios

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,7 +1,20 @@
+const axios = require('axios');
+
 let name = document.getElementById('name');
 let validationResult = document.getElementById('validation-result');
 const validateName = function () {
-    validationResult.innerText = 'Validuosiu su: ' + validationResult.dataset.path;
+    validationResult.innerText = '...';
+    axios.post(validationResult.dataset.path, {input: name.value})
+        .then(function(response) {
+            if (response.data.valid) {
+                validationResult.innerHTML = ":)";
+            } else {
+                validationResult.innerHTML = ":(";
+            }
+        })
+        .catch(function (error) {
+            validationResult.innerText = 'Error: ' + error;
+        });
 };
 
 name.onkeyup = validateName;

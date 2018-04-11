121941da9cd00ed03858f5a0c882f8d54b197c5a
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-04-11 23:49:56 +0300

    Atvaizduojame duomenis puslapyje
    Pradedame nuo "...", nes užsikrauna ne iš karto, kad naudotoja matytų, jog pasikeis.
    
    Kodo generavimu paleisti per scripts/frontend.sh
      yarn run encore dev --watch
    
    Rezultatas matysis atsidarius pusplaį:
      http://127.0.0.1:8000/people
    
    Dokumentacija:
      https://www.w3schools.com/jsref/prop_html_innerhtml.asp
      https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
      https://developer.mozilla.org/en-US/docs/Web/API/Node/innerText
      https://symfony.com/doc/current/frontend/encore/versioning.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
 require('bootstrap');
 const axios = require('axios');
 
+let versionedFileElement = document.getElementById('versionedFile');
 axios.get('/build/manifest.json')
     .then(function(response) {
-        console.log(response.data);
+        versionedFileElement.innerText = response.data['build/js/app.js'];
     })
     .catch(function (error) {
-        console.error(error);
+        versionedFileElement.innerText = 'Error: ' . error;
     });

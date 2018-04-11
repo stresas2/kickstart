b24e57b5b05f75bb4d5cc9d65db1d072017cf879
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-04-12 00:03:03 +0300

    Perduokime duomenis iš Twig'o į JavaScript'ą
    Problema: naudojame React ir axious kodą tik People puslapyje, bet JavaScript įkraunamas visur. Taigi Pagrindiniame puslapyje Console'je metamos klaidos.
    Sprendimas: įterpti JavaScript dalį, kuri būtų virš. Naudojami globalūs JavaScript kintamieji ("var", bet ne "let")
    
    Gerosios praktikos: naudoti data-... parametrus
    Geresnis sprendimas: patikrinti ar elementas egzistuoja
    
    Kodas sukeneruotas per scripts/frontend.sh
      yarn run encore dev --watch
    
    Patikrinimui, ar veikia:
      Tiek pagrindiniame, tiek /people puslapyje Naršyklės Console nerodo klaidos
    
    Dokumentacija:
      https://symfony.com/doc/current/frontend/encore/server-data.html
      https://www.w3schools.com/js/js_datatypes.asp

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 require('bootstrap');
 const axios = require('axios');
 
-let versionedFileElement = document.getElementById('versionedFile');
-axios.get('/build/manifest.json')
-    .then(function(response) {
-        versionedFileElement.innerText = response.data['build/js/app.js'];
-    })
-    .catch(function (error) {
-        versionedFileElement.innerText = 'Error: ' . error;
-    });
+if (typeof usingVersionedFileJs !== "undefined") {
+    let versionedFileElement = document.getElementById('versionedFile');
+    axios.get('/build/manifest.json')
+        .then(function (response) {
+            versionedFileElement.innerText = response.data['build/js/app.js'];
+        })
+        .catch(function (error) {
+            versionedFileElement.innerText = 'Error: '.error;
+        });
+}

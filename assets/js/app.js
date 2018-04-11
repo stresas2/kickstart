a0233d4dc16dbc1a96d3daf1ac21d3f4e0e33643
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-04-11 23:43:52 +0300

    Padarom AJAX užklausą naudojant axios biblioteką
    Čia viena iš pavyzdžių, kaip galima panaudoti išorinę bilbioteką,
    kad mūsų kodas būtų paprastesnis.
    
    Kad kodas veiktų, paleidžiame per scripts/frontend.sh
      yarn run encore dev --watch
    
    Patirikrinimui:
      http://127.0.0.1:8000/people
    
    Patirkinimui, atsidarome Naršyklės console langą
      Turėtų būti kažkas į Object { "build/js/app.js": "/build/js/app.js", "build/css/app.css": "/build/css/app.css" }
    
    Dokumentacija:
      https://www.npmjs.com/package/axios

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,11 @@ $(document).ready(function() {
 });
 
 const axios = require('axios');
+
+axios.get('/build/manifest.json')
+    .then(function(response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.error(error);
+    });

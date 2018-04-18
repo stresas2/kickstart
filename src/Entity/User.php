b6b402dcd8973114c71b33579c307a805cd3e455
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-04-18 19:19:37 +0300

    Paruošiame duomenų bazę FOSUserBundle'ui
    Kickstarter jau turi sukonfiguruotą MySql duomenų bazę, tai belieka nusikopijuoti .env.dist -> .env
    README.md faile yra parašyta "Kaip prisijungti prie MySql duomenų bazės?"
    
    Kodas sugeneruotas scripts/backend.sh:
      bin/console doctrine:database:create
      bin/console doctrine:migrations:diff
      bin/console doctrine:migrations:migrate
    
    doctrine:migrations:diff komanda palygina laukų aprašymus Symfony (PHP) pusėje ir pagal tai sugeneruoja src/Migrations/*.php failą
    Dauguma atveju jis sugeneruoja gerai. Darant realų projektą, prieš leidžiant doctrine:migrations:migrate, peržiūrimas src/Migrations/*.php failas
    
    Patirkinimui, ar veikia:
      Sukuriame naują naudotoją per http://127.0.0.1:8000/register/
      Mus nukreipia į http://127.0.0.1:8000/register/confirmed
      Paspaudžiame Logout – apačioje neberodo, kad prisijungęs
      Prisijungiame per http://127.0.0.1:8000/login
      Vėl apatinėje juostoje matome, kad prisjungta
    
    Duomenų bazėje galima patikrinti:
      SELECT * FROM fos_user;
    
    Dokumentacija:
      https://symfony.com/doc/master/bundles/FOSUserBundle/index.html#step-7-update-your-database-schema
      https://www.doctrine-project.org/projects/doctrine-migrations/en/latest/reference/generating_migrations.html

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,5 +12,10 @@ use FOS\UserBundle\Model\User as BaseUser;
  */
 class User extends BaseUser implements UserInterface
 {
-
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
 }

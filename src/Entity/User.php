924f30ca3a12ae7a438e606b7e80e698462ac93c
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-04-18 21:45:37 +0300

    Pridedame naują laukelį į User klasę
    Anotacijos gali būti sugeneruotos PHPStorm'o (Alt+Insert -> ORM Annotation).
    Set/Get komandos irgi gali būti sugeneruotos su PHPStorm'u (Alt+Insert -> Getters and Setters).
    
    Šis lauka yra privaloma (nėra NULL reikšmės), todėl naudotojo registracija nebeveiks.
    
    Duomenų bazė sugeneruota su scripts/backend.sh:
      bin/console doctrine:migrations:diff
    
    Duomenų bazės pakeitimai įsigalioja scripts/backend.sh:
      doctrine:migrations:migrate
    
    Dokumentacija:
      https://knpuniversity.com/screencast/fosuserbundle/customize-forms

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -18,4 +18,30 @@ class User extends BaseUser implements UserInterface
      * @ORM\Column(type="integer")
      */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $website;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWebsite()
+    {
+        return $this->website;
+    }
+
+    /**
+     * @param mixed $website
+     */
+    public function setWebsite($website): void
+    {
+        $this->website = $website;
+    }
 }

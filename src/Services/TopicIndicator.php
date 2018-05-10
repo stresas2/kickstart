a981787a281901e1966dbc18514624af03b90170
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-05-10 22:46:13 +0300

    Paprastas servisas bandantis atspėti, kokia tema rašo klientas
    Tokiu atveju turime sudėtingesnę logiką, kur yra tiek backend'o dalis,
    tiek jos atvaizdavimas Twig'e.
    
    Testavimui:
     * Įrašius: "I want to buy product A, can I get some discount if I am buying two items?"
     * Įrašius: "My order failed, I saw error message: I cannot deliver to your location"
     * Įrašius: "I found product A, but what is usual battery life?"
     * Įrašius: "Just testing your system"
    
    Naudotos technologijos:
     * https://symfony.com/doc/current/controller/service.html
    
    (cherry picked from commit 9890965)

---
+++ src/Services/TopicIndicator.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Services;
+
+/**
+ * Simple natural text processor, that tries to determine main themes in the text
+ */
+class TopicIndicator
+{
+    const TOPIC_UNKNOWN = 'topic_unknown';
+    const TOPIC_QUESTION = 'topic_question';
+    const TOPIC_ANGRY = 'topic_angry';
+    const TOPIC_BUY = 'topic_buy';
+
+    private $questionIdentifiers = ['why', 'how', 'when', 'can'];
+    private $angryIdentifiers = ['failed', 'error', 'fault', ''];
+    private $buy = ['buy', 'details', 'discount', 'price', 'stock'];
+
+    public function getTopic($text)
+    {
+        $indexes = [
+            self::TOPIC_BUY => 0,
+            self::TOPIC_ANGRY => 0,
+            self::TOPIC_QUESTION => 0,
+        ];
+        $words = preg_split('/((^\p{P}+)|(\p{P}*\s+\p{P}*)|(\p{P}+$))/', strtolower($text), -1, PREG_SPLIT_NO_EMPTY);
+        foreach ($words as $word) {
+            if (in_array($word, $this->questionIdentifiers)) {
+                $indexes[self::TOPIC_QUESTION]++;
+            }
+            if (in_array($word, $this->angryIdentifiers)) {
+                $indexes[self::TOPIC_ANGRY]++;
+            }
+            if (in_array($word, $this->buy)) {
+                $indexes[self::TOPIC_BUY]++;
+            }
+        }
+        if (strpos($text, '?') && $indexes[self::TOPIC_BUY] == 0) {
+            $indexes[self::TOPIC_QUESTION]++;
+        }
+        if (strpos($text, '!')) {
+            $indexes[self::TOPIC_ANGRY]++;
+        }
+
+        asort($indexes);
+        $value = end($indexes);
+        $best = key($indexes);
+
+        if ($value <= 0) {
+            return self::TOPIC_UNKNOWN;
+        }
+
+        return $best;
+    }
+}

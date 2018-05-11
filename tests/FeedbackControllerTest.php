e76ab58e334fd439e9eb019a3486010a2c869cfc
Aurelijus Banelis <aurelijus@banelis.lt>
2018-05-11 16:19:00 +0000

    Pataisome testą, naudojant CSS selectorius
    Imituojame naršyklę Symfony viduje su createClient funkcija.
    Reali naršyklė nėra paleidžiama, bet paleidžiamas kodas taip lyg iš naršyklės.
    Todėl tokie funkciniai testai yra pakankamai greiti
    (greitesni už realios naršyklės paleidimą, lėtesni už modulių testus, kur nereikia užkrauti viso Symfony karkaso)
    
    Elementams atpažinti naudojami CSS selector.
    Kaip apsirašome stilių CSS faile, tokiu pačiu principu tapo populiaru ieškoti ir elementų.
    Anksčiau (kituose įrankiuose) alternatyva yra naudoti XPath.
    
    Patestavimui, ar veikia PHP konteineryje paleidžiame:
     bin/phpunit
    
    Turėtų rodyti, kad testai yra teisingi (žali): OK (1 test, 3 assertions)
    
    Plačiau:
     * https://symfony.com/doc/current/testing.html#functional-tests
     * https://www.w3schools.com/cssref/css_selectors.asp
     * https://developer.mozilla.org/en-US/docs/Tools/Page_Inspector/How_to/Select_an_element
     * https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector

--- tests/FeedbackControllerTest.php
+++ tests/FeedbackControllerTest.php
@@ -6,12 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class FeedbackControllerTest extends WebTestCase
 {
-    public function testSomething()
+    public function testTalkBoxIsOnHomePage()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $this->assertSame(1, $crawler->filter('h1:contains("Hello World")')->count());
+        $this->assertSame(1, $crawler->filter('#feedback-form textarea')->count());
+        $this->assertSame("Send", $crawler->filter('#feedback-form button[type=submit]')->first()->text());
     }
 }

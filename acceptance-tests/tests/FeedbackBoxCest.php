273046190b82b57be83d7a347cf9bb73d649b052
Aurelijus Banelis <aurelijus@banelis.lt>
2018-05-14 19:45:01 +0000

    Codeption: Keletas naudingų komandų
    Testus bandoma prasukti pačiu įmanomu greičiu, bet kartais norisi pamatyti, kas yra daroma:
     * (kol rašomi testai!) įdėti užmigimui funkcijas ($I->wait)
     * (kol rašomi testai!) laukti, kol žmogus paspaus Enter klavišą
    
    Puslapio rezultatas dažniausiai yra testuojamas:
     * Pagal matomą tekstą (pvz. $I->see)
     * Pagal egzistuojančius elementus (CSS selectorius, $I->seeElement)
     * Pagal dinamiškai/JavaScript'u apskaičiuotas reikšmes ($I->executeJS)
    
    Turinys dinamiškai gali užsikrauti ne iš karto, todėl verta naudoti:
     * $I->waitForElement – jei laukiam, kol bus atvaizduotas elementas (paieška pagal CSS seclector)
     * $I->waitForJS – jei elemento egzistavimą galima apskaičiuoti su JavaScript)
     * $I->wait – (blogiausiu atveju, jei nepavyksta kitaip!) laukti kažkurį laiką (kai testų padaugės, norėsisis visus $I->wait pakeisti į $I->waitForElement arba $I->waitForJS)
    
    Palaidžiama PHP konteineryje:
     vendor/bin/codecept run --debug acceptance FeedbackBoxCest
    
    Plačiau:
     * https://codeception.com/docs/01-Introduction
     * https://codeception.com/docs/modules/WebDriver#executeJS
     * https://codeception.com/docs/modules/WebDriver#waitForJS

--- acceptance-tests/tests/FeedbackBoxCest.php
+++ acceptance-tests/tests/FeedbackBoxCest.php
@@ -14,5 +14,17 @@ class FeedbackBoxCest
     // tests
     public function tryToTest(AcceptanceTester $I)
     {
+        $I->amOnPage('/');
+        $I->wait(5); // seconds
+        $I->see('Your feedback');
+        $title = $I->executeJS('return document.title');
+        $I->comment("Testing: " . $title);
+        $I->waitForElement('not > existing#element to.fail', 5); // Fail after 5 seconds timeout
+    }
+
+    // Should be removed if using TravisCI or similar Continuous integration server
+    public function _failed(AcceptanceTester $I)
+    {
+        $I->pauseExecution();
     }
 }

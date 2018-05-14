c91cb583675315fe14a9bb23d73f53940253a1d6
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-05-14 23:19:41 +0300

    Codeception: Parašome realistiškesnį testą
    Testas paleidžiamas PHP konteineryje:
     vendor/bin/codecept run --debug acceptance FeedbackBoxCest::sendProductRequest
    
    Moduliuose galima užrašyti smulekesnes operacija, kad *Cest.php faile kodą skaitant būtų aišku, ką norima daryti.
    Codeception sugeneruoja kodą sujungimui ir su "--debug" parametru gražiai (su tarpais) atspausdina funkcijos pavadinimą.
    Todėl pagal (gerus) funkcijų pavadinimus testo esmę gali suprasti ir netechninis žmogus.
    
    Kad PHPStorm rodytų autocomplete, verta paleisti "vendor/bin/codecept build" komandą,
    kuri sugeneruos failus "_generated" kataloge.
    
    Plačiau:
     * https://codeception.com/docs-2.0/03-ModulesAndHelpers
     * https://codeception.com/docs/modules/WebDriver
    
    (cherry picked from commit a046c39)
    
    (cherry picked from commit ac9c412)

--- acceptance-tests/tests/FeedbackBoxCest.php
+++ acceptance-tests/tests/FeedbackBoxCest.php
@@ -22,6 +22,27 @@ class FeedbackBoxCest
         $I->waitForElement('not > existing#element to.fail', 5); // Fail after 5 seconds timeout
     }
 
+    /**
+     * @param AcceptanceTester $I user
+     * @throws Exception
+     */
+    public function sendProductRequest(AcceptanceTester $I)
+    {
+        $I->am("Interested buyer");
+        $I->amOnPage('/');
+        $inputText = 'I want to buy product A, can I get some discount if I am buying two items?';
+        $I->fillField('#feedback-form textarea', $inputText);
+        $I->click('#feedback-form button[type="submit"]');
+
+        $I->waitForElement('.feedback-results');
+        $I->amRedirectedTo('/feedback/conversation');
+        $I->dontSee('Warning');
+        $I->dontSee('Error');
+        $I->canSee($inputText, '.feedback-results .question');
+        $contactText = 'Thank you for your feedback. Call our sales +370 37 793 515 and get the best deal.';
+        $I->canSee($contactText, '.feedback-results .answer');
+    }
+
     // Should be removed if using TravisCI or similar Continuous integration server
     public function _failed(AcceptanceTester $I)
     {
--- acceptance-tests/tests/_support/Helper/Acceptance.php
+++ acceptance-tests/tests/_support/Helper/Acceptance.php
@@ -4,7 +4,15 @@ namespace Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
+use Codeception\Module\WebDriver;
+
 class Acceptance extends \Codeception\Module
 {
-
+    public function amRedirectedTo($path)
+    {
+        /** @var WebDriver $webDriver */
+        $webDriver = $this->getModule('WebDriver');
+        $actual = $webDriver->executeJS('return window.location.pathname');
+        $this->assertEquals($path, $actual, 'Browser path does not match expected');
+    }
 }

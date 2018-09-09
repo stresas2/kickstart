87968bdd37669824b0c5b3d4f2e9a3a99d5a2688
Aurelijus Banelis <aurelijus@banelis.lt>
2018-09-09 20:40:07 +0000

    Debug package for Twig dump
    Was using:
    composer require --dev debug

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];

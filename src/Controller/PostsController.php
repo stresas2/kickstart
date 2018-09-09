23a81cda196d2351bf265d8379f04923efbd225e
Aurelijus Banelis <aurelijus@banelis.lt>
2018-09-09 21:18:52 +0000

    Happy path to store Post
    Also fixed name for $post object.

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Form\PostType;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -25,21 +26,33 @@ class PostsController extends Controller
     /**
      * @Route("/posts/new", name="new_posts")
      */
-    public function newPost(Request $request, Session $session, UserInterface $user = null)
+    public function newPost(Request $request, Session $session, ObjectManager $entityManager, UserInterface $user = null)
     {
         if (!$user) {
             return $this->redirectToRoute('fos_user_security_login');
         }
 
-        $user = new Post('', new \DateTime(), '', $user);
-        $form = $this->createForm(PostType::class, $user);
+        $post = $this->emptyPost($user);
+        $form = $this->createForm(PostType::class, $post);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var Post $post */
             $post = $form->getData();
+            $this->storePost($post, $entityManager);
             $session->getFlashBag()->add('success', "Post will be saved: " . $post->getTitle());
         }
 
         return $this->render('posts/new.html.twig', ['form' => $form->createView()]);
+    }
+
+    private function storePost(Post $post, ObjectManager $entityManager)
+    {
+        $entityManager->persist($post);
+        $entityManager->flush();
+    }
+
+    private function emptyPost(UserInterface $user)
+    {
+        return new Post('', new \DateTime(), '', $user);
     }
 }

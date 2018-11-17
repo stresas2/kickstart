ee37e341a3749ef3fd1b943e1700294780a01c25
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-11-17 21:30:09 +0200

    Atnaujinkime iki Encore 0.21.0
    Nes senoje versijoje axios buvo dedamas ir į app.js, ir į validation.js failus.
    
    Nes kol pradėjome mokytis, ekosistema jau pažengė į priekį
    
    Atnaujinta pagal dokumentaciją:
      https://symfony.com/blog/encore-0-21-0-webpack-4-support-copy-files-support-webpackencorebundle
      https://stackoverflow.com/questions/53230674/unknown-encore-entry-link-tags-function-when-using-webpack-encore-0-21-0
    
    Kitos naudingos nuorodos:
      https://symfony.com/doc/current/frontend/encore/split-chunks.html
      https://www.robinwieruch.de/minimal-react-webpack-babel-setup/#babel-react-setup

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,8 @@ Encore
     .addStyleEntry('css/app', './assets/css/app.scss')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG

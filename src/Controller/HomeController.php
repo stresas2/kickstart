a2a74945d7a90da3566089a6dfd2bcaf82d10821
Aurelijus Banelis <aurelijus@banelis.lt>
2018-11-26 19:18:55 +0200

    Paprasčiausias būdas pridėti Service
    Tiesiog sukuriame naują kalsę.
    
    Kadangi service.yaml yra:
        # makes classes in src/ available to be used as services
        # this creates a service per class whose id is the fully-qualified class name
        App\:
            resource: '../src/*'
            exclude: '../src/{DependencyInjection,Entity,Migrations,Tests,Kernel.php}'
    
    Todėl visi failai esantys po App\ namespace'u yra iškviečiami.
    
    Patirkinimui:
      http://127.0.0.1:8000/ turėtų rodyti 3
    
    Patikrinimui scripts/backend.sh:
      bin/console debug:autowiring MyServic
      bin/console debug:container 'App\Services\MyService'
    
    Dokumentacija:
      https://symfony.com/doc/current/service_container.html

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Services\MyService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends Controller
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(MyService $myService)
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'sum' => $myService->sum(1, 2),
         ]);
     }
 }
---
+++ src/Services/MyService.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Services;
+
+class MyService
+{
+    public function sum($a, $b) {
+        return $a + $b;
+    }
+}

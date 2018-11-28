f826533f77777461c55f33bcc0618f5dadfc1a5f
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-11-29 00:16:35 +0200

    Pradedame galvoti apie įgyvendinimą pradžiai parašę testą (TDD metodika)
    Test Driven Design (TDD) yra gan populiari metodika, kai pažingsniui tobulinamas funckionalumas.
    
    Patikrinimui scripts/backend.sh
      bin/phpunit
    
    Dokumentacija:
      https://en.wikipedia.org/wiki/Test-driven_development
      https://phpunit.readthedocs.io/en/8.1/assertions.html

--- tests/NfqTeamsTest.php
+++ tests/NfqTeamsTest.php
@@ -13,4 +13,40 @@ class NfqTeamsTest extends TestCase
         $this->assertNull($teams->getTeamByMember('notExisting'));
         $this->assertNull($teams->getTeamByMentor('notExisting'));
     }
+
+    public function testValidMentor()
+    {
+        $teams = new NfqTeams(
+            [
+                'academyui' => [
+                    'mentors' => ['Jonas Jonaitis'],
+                    'students' => ['Petras Petraitis', 'Gedas Gražauskas']
+                ],
+                'supperreal' => [
+                    'mentors' => ['Vardenis Pavardenis', 'Ada Kalbenė'],
+                    'students' => ['Vytautas Vėjūnas']
+                ],
+            ]
+        );
+        $this->assertEquals('academyui', $teams->getTeamByMentor('Jonas Jonaitis'));
+        $this->assertEquals('supperreal', $teams->getTeamByMentor('Ada Kalbenė'));
+    }
+
+    public function testValidMember()
+    {
+        $teams = new NfqTeams(
+            [
+                'academyui' => [
+                    'mentors' => ['Jonas Jonaitis'],
+                    'students' => ['Petras Petraitis', 'Gedas Gražauskas']
+                ],
+                'supperreal' => [
+                    'mentors' => ['Vardenis Pavardenis', 'Ada Kalbenė'],
+                    'students' => ['Vytautas Vėjūnas']
+                ],
+            ]
+        );
+        $this->assertEquals('academyui', $teams->getTeamByMember('Gedas Gražauskas'));
+        $this->assertEquals('supperreal', $teams->getTeamByMember('Vytautas Vėjūnas'));
+    }
 }

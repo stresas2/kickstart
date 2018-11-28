a30472fd7c6d2b60245774c8b2e4e824e3daf76a
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-11-28 23:57:34 +0200

    Pasidarome paparastą klasę, kurią naudosime testavimui
    Ji ji turi įgyvendinti mūsų sutartą interface'ą.
    Mes nenorėjome įkelti vardų tiesiai į kodą, todėl paduodame per konstruktorių.

---
+++ src/Services/NfqTeams.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+class NfqTeams implements TeamsInterface
+{
+    private $data = [];
+
+    /**
+     * NfqKaunasTeams constructor.
+     *
+     * @param string[][] $data ['teamName' => ['mentors' => ..., 'students' => ''], ...]
+     */
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function getTeamByMember(string $name): ?string
+    {
+        return null;
+    }
+
+    public function getTeamByMentor(string $name): ?string
+    {
+        return null;
+    }
+}

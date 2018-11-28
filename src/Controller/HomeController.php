f58a08ba204cb45c4e42ba3f9fea11d2ca2d4fce
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-11-29 00:57:10 +0200

    Kol vienas kolega tikrino logiką, kitas spėjo padaryti dizainą
    Kai sujungiame abi dalis – viskas veikia ir jaučiamės užtikrinti,
    nes turime testus.
    
    Patikrinimui atsidarome http://127.0.0.1:8000/ ir paspaudę ant nuorodos rodo, kad "Vardenis Pavardenis yra academyui komandoje"
    
    Dokumentacija:
      https://symfony.com/doc/current/service_container.html
      https://symfony.com/doc/current/service_container.html
      https://symfony.com/doc/current/components/templating.html

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Services\TeamsInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -12,8 +14,17 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/member/{member}", name="member")
+     */
+    public function member(TeamsInterface $teams, string $member = "")
+    {
+        return $this->render('home/member.html.twig', [
+            'member' => $member,
+            'team' => $teams->getTeamByMember($member)
         ]);
     }
 }

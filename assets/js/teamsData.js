a83b9df357eec9226b5d5c07a9131160c2b02024
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-11-29 16:06:51 +0200

    Pasirašome paprastą funkcijų rinkinį ir jam skirtą testą
    Testinius duomenis naudojame kaip atskirą failą,
    tik kad būtų paprastesnis pavyzdys.
    
    Testų paleidimui scripts/frontend.sh
      yarn test
    
    Documentacija:
      https://jestjs.io/docs/en/getting-started
      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import

---
+++ assets/js/teamsData.js
@@ -0,0 +1,22 @@
+const teamsData = {
+    "academyui": {
+        "mentors": [
+            "Jonas Jonaitis"
+        ],
+        "students": [
+            "Petras Petraitis",
+            "Vardenis Pavardenis"
+        ]
+    },
+    "supperreal": {
+        "mentors": [
+            "Giedrius Gerulis"
+        ],
+        "students": [
+            "Jonas Jaunasis",
+            "Vytautas Vėjūnas"
+        ]
+    }
+};
+
+export {teamsData};

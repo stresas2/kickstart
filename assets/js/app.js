e7ce5df9a7d566ad965741d72c5c56a745f8a1a3
Aurelijus Banelis <aurelijus.banelis@home24.de>
2018-11-29 16:10:29 +0200

    Pasitikriname, ar visa tai vis dar veikia per naršyklę
    app.js paima komponentą taip pat kaip ir testų failas.
    Dėl pavyzdžio paprastumo – naudoju Vanilla JavaScript.
    
    JavaScript failų sugeneravimui scripts/frontend.sh
      yarn encore dev
    
    Atsidarius http://127.0.0.1:8000/member/Vardenis%20Pavardenis matome dinamiškai sugeneruotą:
      Giedriaus komanda: supperreal
    
    Documentacija:
      https://symfony.com/doc/current/frontend.html
      https://symfony.com/doc/current/frontend/encore/babel.html#creating-a-babelrc-file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+import {NfqTeams} from "./nfqTeams";
+import {teamsData} from "./teamsData";
+
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
@@ -11,4 +14,9 @@ require('../css/app.scss');
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
+    let data = teamsData;
+    let domElement = document.getElementById('dynamicMember');
+    if (NfqTeams && domElement) {
+        domElement.innerHTML = 'Giedriaus komanda: ' + NfqTeams.getTeamByMentor(data, 'Giedrius Gerulis');
+    }
 });

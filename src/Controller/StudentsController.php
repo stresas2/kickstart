2f22583167f45933fd5ab8a74fcf683e128d5674
Aurelijus Banelis <aurelijus.banelis@home24.de>
2019-04-18 18:08:15 +0300

    Naudojame symfony dependency injection
    Adresu /students?testas=akademijos keičiasi informacija
    
    https://symfony.com/doc/current/routing/extra_information.html

--- src/Controller/StudentsController.php
+++ src/Controller/StudentsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StudentsController extends AbstractController
@@ -10,10 +11,10 @@ class StudentsController extends AbstractController
     /**
      * @Route("/students", name="students")
      */
-    public function index()
+    public function index(Request $request)
     {
         return $this->render('students/index.html.twig', [
-            'controller_name' => 'StudentsController',
+            'controller_name' => $request->get('testas', 'standartinė'),
         ]);
     }
 }

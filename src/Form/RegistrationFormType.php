0fa55bf9fa49c6956336adcb8d3d2abd38cbcf8b
Aurelijus Banelis <aurelijus@banelis.lt>
2019-05-06 20:40:57 +0300

    Naujų laukų pridėjimas: Papildome formas ir kitus dizaino elementus
    Symfony naudoja formas, kad būtų galima sugeneruoti
    visus patirkinimus (validacijas) daug paprasčiau.
    
    Twig'e visi getter'iai veikia ir be "get" dalies (getHomepage -> homepage)
    
    Kadangi pridedame naują lauką – tai naudojame NULL senoms duomenų bazės eilutėms.
    Standartiškai visi formos laukai yra privalomi – tai pakeičiame,
    kad svetainės adresas būtų nebūtinas.
    
    Dokumentacija:
      https://symfony.com/doc/current/forms.html
      https://symfony.com/doc/current/reference/forms/types/form.html#required

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -34,6 +35,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('homepage', UrlType::class, ['required' => false])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [

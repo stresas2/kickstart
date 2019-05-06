535b0262e6aab561c90300c1d2a8702c2672c797
Aurelijus Banelis <aurelijus@banelis.lt>
2019-05-06 13:55:38 +0300

    Naudotojo objekto pasiekimas iš Controller'io
    Patogu, jei norima nukreipti į Prisijungimą arba neužtenka standartinių Security konfiguracijos galimybių.
    Naudotojo objektas įdedamas į kontrolerio metodą (analogiškai kaip Service'ams).
    Alternatyva būtų naudoti "$this->getUser()", bet naudojant per argumentus –
    lengviau bus perkeleti į kitą projektą (ar paruošti testams).
    
    Nors pavyzdžiuose yra Security $security, $security->getUser(),
    bet praktikoje kitos funkcijos (credentials, attributes) rečiau naudojamas
    nei tiesiogiai kviečiamas UserInterface.
    
    Jei naudotojas(-a) neprisijungęs, reikia "UserInterface $user = null",
    kitaip Symfony nežinos ką daryti su null (neprisijungęs), kurio negalima paversti į UserInterface objektą.
    
    Pastaba:
      AccessDeniedException yra standartinis atsakymas, kai neprisiregistruojama, bet tada grąžinas HTTP 500 statusas.
      Produkcinėje aplinkoje reikėtų reaguoti į kernel.exception arba užsidėti templates/bundles/TwigBundle/Exception/error.html.twig žmogiškai klaidos žinutei
    
    Dokumentacija:
      https://symfony.com/doc/current/security.html#b-fetching-the-user-from-a-service
      https://symfony.com/doc/current/controller.html#redirecting
      https://symfony.com/blog/new-in-symfony-3-2-user-value-resolver-for-controllers
      https://symfony.com/doc/current/security.html#securing-controllers-and-other-code

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -32,5 +36,28 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function profile(UrlGeneratorInterface $urlGenerator, UserInterface $user = null)
+    {
+        if ($user instanceof User) {
+            return $this->render('security/profile.html.twig', [
+                'user' => $user,
+                'userId' => $user->getId(),
+                'roles' => $this->roleNames($user->getRoles()),
+            ]);
+        }
+        // Redirect for not logged in users (or different kind)
+        return new RedirectResponse($urlGenerator->generate('app_login'));
+    }
+
+    private function roleNames(array $userRoles)
+    {
+        foreach ($userRoles as $role) {
+            yield str_replace('ROLE_', '', $role);
+        }
     }
 }

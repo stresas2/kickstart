c7520646ffe4718e246d8106eef79da1d65b8e44
Aurelijus Banelis <aurelijus@banelis.lt>
2019-05-06 12:11:46 +0300

    Pataisome nukreipimą po prisijungimo
    Nukreipiame prisjungusį naudotoją į pagrindinį puslapį.
    Survarkome eilutes, kad atitiktų kodo stilių.
    
    Prisijungimas veikia, bet nėra nei Atsijungti mygtuko,
    nei automatinio prisjungimo po registracijos.
    
    Dokumentacija:
      https://symfony.com/doc/current/security/form_login_setup#finishing-the-login-form

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -28,8 +28,12 @@ class AppCustomAuthenticator extends AbstractFormLoginAuthenticator
     private $csrfTokenManager;
     private $passwordEncoder;
 
-    public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager, UserPasswordEncoderInterface $passwordEncoder)
-    {
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        UrlGeneratorInterface $urlGenerator,
+        CsrfTokenManagerInterface $csrfTokenManager,
+        UserPasswordEncoderInterface $passwordEncoder
+    ) {
         $this->entityManager = $entityManager;
         $this->urlGenerator = $urlGenerator;
         $this->csrfTokenManager = $csrfTokenManager;
@@ -85,8 +89,7 @@ class AppCustomAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()

646ab084f5da9251d17d2d3465009b27ee8c519b
Aurelijus Banelis <aurelijus@banelis.lt>
2019-05-06 20:33:34 +0300

    Naujų laukų pridėjimas: Atnaujiname duomenų bazę
    Naujiems laukams reikia naujų stulpelių duomenų bazės lentelėse.
    
    Symfony turi MigrationBundle, kuris gali sugeneruoti migraciją
    (SQL užklausas) pagal Entity ir dabartinės duomenų bazės skirtumą.
    
    Sugeneravimui naudojama dabartinė data ir reikšmė "migration_versions",
    todėl atkrartojant skirtinga tvarka gali nesuveikti
    (daroma prielaida, kad migracijos yra sukurtos pagal datą viena po kitos).
    
    Sugeneruota naudojant scripts/backend.sh
      bin/console doctrine:migrations:diff
    
    Duomenų bazė atnaujinta naudojant scripts/backend.sh
      bin/console doctrine:migrations:diff
    
    Dokumentacija:
      https://www.doctrine-project.org/projects/doctrine-orm/en/2.6/cookbook/working-with-datetime.html

---
+++ src/Migrations/Version20190507164444.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190507164444 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user ADD password_changed DATETIME DEFAULT NULL, ADD homepage VARCHAR(255) DEFAULT NULL, CHANGE roles roles JSON NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user DROP password_changed, DROP homepage, CHANGE roles roles LONGTEXT NOT NULL COLLATE utf8mb4_bin');
+    }
+}

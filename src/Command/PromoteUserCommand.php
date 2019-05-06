1e0580476987b468211ffaf4c88f70d5ca2a60c9
Aurelijus Banelis <aurelijus@banelis.lt>
2019-05-06 15:51:51 +0300

    Įgyvendinam komandą rolei pakelti
    Per komandinę eilutę pridedame ROLE_ADMIN rolę registruotam naudotojui –
    taip apsaugome administravimo panelę nuo įsilaužėlių.
    
    UserRepository klasėje pasidarome papildomą metodą,
    kad būtų patogiau ieškoti elemento (duomenų bazės įžanga).
    
    Patikrinimui per scripts/backend.sh
      bin/console app:promote-user --help
      bin/console app:promote-user aurelijus@banelis.lt
    
    Ir tada bandome užeiti į http://127.0.0.1:8000/admin
    
    Pakeitus rolę – gali reikėti iš naujo prisijungti,
    nes naudotojo pasikeitimas tikrinamas pagal serialize
    metodą su išsaugota versija Sesijoje.
    
    Dokumentacija:
      https://symfony.com/doc/current/components/console.html
      https://symfony.com/doc/3.4/doctrine/repository.html
      https://symfony.com/doc/current/security.html#roles
      https://symfony.com/doc/current/security/user_provider.html#understanding-how-users-are-refreshed-from-the-session

--- src/Command/PromoteUserCommand.php
+++ src/Command/PromoteUserCommand.php
@@ -2,39 +2,88 @@
 
 namespace App\Command;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class PromoteUserCommand extends Command
 {
     protected static $defaultName = 'app:promote-user';
 
+    /** @var EntityManagerInterface */
+    private $entityManager;
+    private $adminRole;
+
+    /**
+     * PromoteUserCommand constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager, $adminRole = 'ROLE_ADMIN')
+    {
+        $this->entityManager = $entityManager;
+        $this->adminRole = $adminRole;
+
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this
-            ->setDescription('Add a short description for your command')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
+            ->setDescription('Make user admin')
+            ->addArgument('email', InputArgument::REQUIRED, 'E-mail address of existing user');
     }
 
+    /**
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        // Parsing input
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $email = $input->getArgument('email');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        // Getting user
+        $this->info("Searching for user", $email, $io);
+        $user = $this->entityManager->getRepository(User::class)->findByEmail($email);
+        if (!$user) {
+            $io->error("Cannot find user by e-mail: " . $email);
+            return;
         }
 
-        if ($input->getOption('option1')) {
-            // ...
+        if (in_array($this->adminRole, $user->getRoles())) {
+            $this->printUserRoles($user, $io);
+            $io->success('Admin role already exists');
+            return;
         }
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        // Adding admin role
+        $this->info("Adding role: ", $this->adminRole, $io);
+        $roles = $user->getRoles();
+        $roles[] = $this->adminRole;
+        $user->setRoles(array_unique($roles));
+
+        // Storing user
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $this->printUserRoles($user, $output);
+        $io->success('Admin role successfully added');
+    }
+
+    private function info($message, $value, OutputInterface $io)
+    {
+        $io->writeln(sprintf('<info>%s</info>: <comment>%s</comment>', $message, $value));
+    }
+
+    private function printUserRoles(UserInterface $user, OutputInterface $io)
+    {
+        $io->writeln(
+            "<info>User roles</info>: <comment>" . join('</comment>, <comment>', $user->getRoles()) . '</comment>'
+        );
     }
 }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -19,6 +19,21 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
+    /**
+     * @param string $email
+     * @return User|null
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function findByEmail($email): ?User
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.email = :email')
+            ->setParameter('email', $email)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+
     // /**
     //  * @return User[] Returns an array of User objects
     //  */

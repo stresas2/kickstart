41878c5467db35b4e68788b62fa6d795f3cb2b0f
Aurelijus Banelis <aurelijus@banelis.lt>
2019-11-10 18:47:44 +0200

    Tests for Symfony 1 Homework
    Intention is to create similar project in symfony,
    as is deployed on https://hw1.nfq2019.online
    
    Note: https://hw1.nfq2019.online/students.json can be useful.

--- tests/e2e/cypress/integration/symfony-hw1.js
+++ tests/e2e/cypress/integration/symfony-hw1.js
@@ -10,8 +10,73 @@ const dd = async (message) => {
 };
 
 describe('First homework', function() {
+    it('Have students block', () => {
+        cy.visit(basePath);
+        cy.contains("Studentai")
+            .parent()
+            .children('.list-group-item:not(.list-group-item-info)')
+            .then(data => data.map((index, element) => element.innerText).get())
+            .then(students => {
+                cy.wrap(students).each(student => dd(`Student: ${student}`));
+            }).then( students => {
+                assert.equal(students.length, 46, "Expected 46 elements of students");
+            }).then( students => {
+                cy.wrap(students).each(e => {
+                    expect(e).contain("Mentorius");
+                })
+            });
+
+        cy.contains("Studentai").parent().screenshot();
+    });
+    it('Have correct students-mentor data', () => {
+        cy.visit(basePath);
+        cy.contains("Studentai")
+            .parent()
+            .children('.list-group-item:not(.list-group-item-info)')
+            .first()
+            .contains("Tadas")
+            .parent()
+            .contains("Mantas");
+    });
+
+    it('Have project block', () => {
+        cy.visit(basePath);
+        cy.contains("Projektai").parent().screenshot();
+    });
+
+    it('Have form block', () => {
+        cy.visit(basePath);
+        cy.contains("Sužinoti vertinimą").parent().screenshot();
+    });
+
     it('Whole page', () => {
         cy.visit(basePath);
         cy.screenshot();
+    });
+
+    it('Click on first student', () => {
+        cy.visit(basePath);
+        cy.contains("Studentai")
+            .parent()
+            .children('.list-group-item:not(.list-group-item-info)')
+            .first().children('a').click();
+
+        d("Expected inner page");
+        cy.contains("Studentas");
+        cy.contains("Projektas");
+        cy.screenshot();
+
+        cy.contains("Visi studentai").click()
+    });
+
+    it('Click on Data file', () => {
+        cy.visit(basePath);
+        cy.contains("Duomenų failas").then( a => {
+            const link = a.attr('href');
+            cy.wrap([link]).each(link => dd(`Data file link: ${link}`))
+        }).then( links => {
+            const link = links[0];
+            expect(link).to.eq("students.json");
+        })
     });
 });

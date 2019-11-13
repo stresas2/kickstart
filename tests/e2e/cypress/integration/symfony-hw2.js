69dec24c693b0cc01a9f1739576d88165074deb5
Aurelijus Banelis <aurelijus.banelis@home24.de>
2019-11-13 23:20:00 +0200

    Main test cases
    Assuming element IDs are correct.

--- tests/e2e/cypress/integration/symfony-hw2.js
+++ tests/e2e/cypress/integration/symfony-hw2.js
@@ -9,6 +9,8 @@ const dd = async (message) => {
     await cy.ciLog(message);
 };
 
+const second = 1000;
+
 describe('Secodn homework', function() {
     it('Whole page: Homepage', () => {
         cy.visit(basePath);
@@ -16,6 +18,33 @@ describe('Secodn homework', function() {
     });
     it('Whole page: People', () => {
         cy.visit(`${basePath}/people`);
+        cy.screenshot();
+    });
+
+    it('Test old functionality: happy case', () => {
+        cy.visit(`${basePath}/people`);
+        cy.get("#name").type("lukas");
+        cy.wait(second);
+        cy.get("#validation-result-name").contains(":)")
+    });
+    it('Test old functionality: not existing', () => {
+        cy.visit(`${basePath}/people`);
+        cy.get("#name").type("neegzistuojantis");
+        cy.wait(second);
+        cy.get("#validation-result-name").contains(":(")
+    });
+    it('Test new functionality: happy case', () => {
+        cy.visit(`${basePath}/people`);
+        cy.get("#team").type("devcollab");
+        cy.wait(second);
+        cy.get("#validation-result-team").contains(":)");
+        cy.screenshot();
+    });
+    it('Test new functionality: not existing', () => {
+        cy.visit(`${basePath}/people`);
+        cy.get("#team").type("neegzistuojanti");
+        cy.wait(second);
+        cy.get("#validation-result-team").contains(":(");
         cy.screenshot();
     });
 });
